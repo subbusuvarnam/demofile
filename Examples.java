1871f88c7d1f46eda1900136967765f277ceaf94
subbusuvarnam <109512897+subbusuvarnam@users.noreply.github.com>
2022-08-01 15:26:26 +0530

    Add files via upload

---
+++ Examples.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CP1LAb-49
+ */
+public class Examples {
+    System.out.print("hello subbu");
+    
+}
